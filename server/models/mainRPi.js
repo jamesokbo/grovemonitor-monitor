6d08032a16a6899af99d40d198db797a70c1b1c7
jamesokbo <jamesokbo@gmail.com>
2016-09-27 17:15:41 -0500

    Update mainRPi.js

--- server/models/mainRPi.js
+++ server/models/mainRPi.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
 var mainRPiSchema= mongoose.Schema({
-    name:String, /* Assigned by the user to identify his Grove */
+    mainRPiID:String, /*Assigned by the server on identification*/
     userID:String, /* The ID of the user to whom this system belongs to */
+    name:String, /* Assigned by the user to identify his Grove */
     status: Boolean, /* true if connected to the server */
     lastConnection: Date,
     tempUnit:String /* Defined by the user, value is either 'Celsius' or 'Fahrenheit' */
