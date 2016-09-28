e059a7e50ef11f4295c147b5d5caf34f192b7b8f
jamesokbo <jamesokbo@gmail.com>
2016-09-28 16:55:51 -0500

    Create socketToServer.js

---
+++ socketToServer.js
@@ -0,0 +1,43 @@
+var fs=require('fs');
+var constants= require('./constants.js')
+
+module.exports = function(serverURL){
+  var io=require('socket.io-client')(URL);
+  io.on('connect',function(){
+    fs.readFile(constants.MAIN_ID_PATH,'utf8',function(err,data){
+      if(err){
+        console.log(err);
+        throw err;
+      }
+      constants.mainID=data;
+      io.emit('identification', {mainID: constants.mainID}, function(err,res){
+        if(err){
+          console.log(err);
+          throw err;
+        }
+        if(res.status){
+          if(res.new){
+            fs.writeFile(constants.MAIN_ID_PATH, res.id, 'utf8', function(err){
+              if(err){
+                console.log(err);
+                throw err;
+              }
+              constants.mainID=res.id;
+              io.disconnect();
+            });
+          }
+          else{
+            constants.connectionStatus=true;
+          }
+        }
+        else{
+          if(res.error){
+            console.log(res.error);
+          }
+          socketToServer.disconnect();
+        }
+      });
+    });
+  });
+  
+}
