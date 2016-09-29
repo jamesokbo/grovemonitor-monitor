8da01257c50b3057a1085f7bd684078bde3d4233
jamesokbo <jamesokbo@gmail.com>
2016-09-29 15:41:50 -0500

    Update mReading.js

--- sockets/serverSocket/mReading.js
+++ sockets/serverSocket/mReading.js
@@ -1,5 +1,6 @@
-var constants= require('../../../constants.js')
-var envVariables=require('../../../envVariables.js')
+var constants= require('../../../constants.js');
+var errors= require('../../../errors.js');
+var envVariables=require('../../../envVariables.js');
 
 module.exports = function(socket){
   socket.on('mReading',function(data,fn){
@@ -7,27 +8,27 @@ module.exports = function(socket){
       if(envVariables.resMonitorsIDs.indexOf(data.resMonitorID)!=-1){
         envVariables.resMonitors[resMonitorsIDs.indexOf(data.resMonitorID)].emit('mReading',data,function(err,ans){
           if(err){
-            console.log(err);
-            throw err;
+            fn(err);
           }
           fn(ans);
         });
       }
       else{
-        //Error: The requested monitor has lost connection, please verify connectivity
+        fn(errors.m001);
       }
     }
     else{
       if(envVariables.envMonitorsIDs.indexOf(data.envMonitorID)!=-1){
         envVariables.envMonitors[resMonitorsIDs.indexOf(data.envMonitorID)].emit('mReading',data,function(err,ans){
           if(err){
-            console.log(err);
-            fn(err)
-            throw err;
+            fn(err);
           }
           fn(ans);        
         });
       }
+      else{
+        fn(errors.m001);
+      }
     }
-});
+  });
 }
