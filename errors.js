b6c520935822b6ae6f43fcb0da68d38eeff698cf
jamesokbo <jamesokbo@gmail.com>
2016-09-29 16:34:42 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -1,3 +1,4 @@
 module.exports={
-  m001: Error('rm001: The requested monitor has lost connection')
+  m001: Error('m001: The requested monitor has lost connection'),
+  m002: Error('m002: The requested monitor isnt recognized by this system')
 }
