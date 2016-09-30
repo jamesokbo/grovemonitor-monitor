e1f63886d1d8dfc34a8b8b2e0235f0fa1164cc98
jamesokbo <jamesokbo@gmail.com>
2016-09-30 11:15:43 -0500

    Update connection.js

--- sockets/serverSocket/connection.js
+++ sockets/serverSocket/connection.js
@@ -6,20 +6,17 @@ module.exports = function(socket){
   socket.on('connect',function(){
     fs.readFile(constants.MAIN_ID_PATH,'utf8',function(err,data){
       if(err){
-        console.log(err);
         throw err;
       }
       constants.MAIN_ID=data;
       socket.emit('identification', {mainID: constants.MAIN_ID}, function(err,res){
         if(err){
-          console.log(err);
           throw err;
         }
         if(res.status){
           if(res.new){
-            fs.writeFile(constants.MAIN_ID_PATH, res.id, 'utf8', function(err){
+            fs.writeFile(constants.MAIN_ID_PATH, res.mainID, 'utf8', function(err){
               if(err){
-                console.log(err);
                 throw err;
               }
               constants.MAIN_ID=res.id;
