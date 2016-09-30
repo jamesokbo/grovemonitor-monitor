49dd60d4cf11fc868702394c0b2841de48f272bf
jamesokbo <jamesokbo@gmail.com>
2016-09-30 11:55:33 -0500

    Update reading.js

--- server/models/reading.js
+++ server/models/reading.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
 
 var readingSchema= mongoose.Schema({
-	resMonID:String,
-	type:String,
+	monID:String,
+	readingType:String,
 	compensated:{type:Boolean,default:false}, 
 	reading:{type:Number,default:0}, 
 	date:{type:Number,default:Date.now()}
 });
 
-module.exports=mongoose.model('Ph',phSchema);
+module.exports=mongoose.model('Reading',readingSchema);
