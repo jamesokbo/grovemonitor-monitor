d1770daa75817fd6a85f34562dd694fd74134404
jamesokbo <jamesokbo@gmail.com>
2016-09-30 13:50:41 -0500

    Update reading.js

--- server/models/reading.js
+++ server/models/reading.js
@@ -3,7 +3,7 @@ var Schema=mongoose.Schema;
 
 var readingSchema= mongoose.Schema({
 	monID:String,
-	readingType:String,
+	readingType:String, //Value: 'pH','EC', 'wTemp', 'wLevel', 'DO', 'aTemp', 'RH', 'lux', 'CO2'
 	compensated:{type:Boolean,default:false}, 
 	reading:{type:Number,default:0}, 
 	date:{type:Number,default:Date.now()}
