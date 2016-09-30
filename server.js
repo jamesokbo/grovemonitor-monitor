92d989bb6dbb41b47fce6a0f1afc5a8e23492336
jamesokbo <jamesokbo@gmail.com>
2016-09-30 12:29:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var async= require('async');
 var express =require('express');
 var app=express();
 
-var http1= require('http').Server(app);
-var http2= require('http').Server(app);
-var envMonitorIO=require('socket.io')(http1);
-var resMonitorIO=require('socket.io')(http2);
+var http= require('http').Server(app);
+var monitorIO=require('socket.io')(http1);
 var serverURL='www.grovemonitor-jamesokbo.c9users.io:8081';
 
 var mongoose= require('mongoose');
@@ -16,10 +14,7 @@ mongoose.connect(configDB.url);
 //MONGOOSE SCHEMAS
 
 //Mongoose ResMonitor and its sensor readings' schemas
-var ResMonitor=require('./server/models/resMonitor');
-var PhReading=require('./server/models/phReading');
-//TODO: Missing ResMonitor Sensor readings
-//TODO: Mongoose EnvMonitor and its sensor readings' schemas
+var Monitor=require('./server/models/monitor');
 //TODO: Add Actuators arrays and schemas
 //TODO: ADD SOCKETIO COMMUNICATIONS
 
@@ -32,24 +27,18 @@ require('./sockets/serverSocket/connection.js')(serverSocket);
 //TODO: Add SocketIO communication protocol with the resMonitor
 //TODO: Add SocketIO communication protocol with the actuators
 
-ResMonitor.update({},{$set:{status:false}},{multi:true},function(err,res){
+Monitor.update({},{$set:{status:false}},{multi:true},function(err,res){
   if(err){
     throw err;
   }
   if(res.ok==1){
-    EnvMonitor.update({},{$set:{status:false}},{multi:true},function(err,res){
+    Monitor.update({},{$set:{status:false}},{multi:true},function(err,res){
       if(err){
         throw err;
       }
-      console.log(res.ok +' '+res.nModified);
-      if(res.ok==1){
-        http1.listen(8080,function(){
-          console.log('envMonitor socketserver running @ port: 8080');
-        });
-        http2.listen(8081,function(){
-          console.log('resMonitor socketserver running @ port: 8081');
-        });
-      }
+      http.listen(8080,function(){
+        console.log('Monitor socketserver running @ port: 8080');
+      });
     });
   }
 });
