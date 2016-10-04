133317d23217b188b9168fcaa1a680bc39f1ecf6
jamesokbo <jamesokbo@gmail.com>
2016-10-04 16:44:35 -0500

    Update reading.js

--- server/models/reading.js
+++ server/models/reading.js
@@ -3,6 +3,7 @@ var Schema=mongoose.Schema;
 
 var readingSchema= mongoose.Schema({
 	monitorID:String,
+	status: Boolean,
 	type:String, //Value: 'ph','ec', 'wTemp', 'wLevel', 'do', 'aTemp', 'rh', 'lux', 'co2'
 	compensated:{type:Boolean,default:false}, 
 	reading:{type:Number,default:0}, 
