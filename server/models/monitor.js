dd79a36a9acb6aae91ce818e6c420a07ed3061aa
jamesokbo <jamesokbo@gmail.com>
2016-10-05 09:57:34 -0500

    Update monitor.js

--- server/models/monitor.js
+++ server/models/monitor.js
@@ -2,33 +2,23 @@ var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
 var monitorSchema= mongoose.Schema({
     monitorID:String, /*ID assigned by the mainRPi on first 'identification', kept as a .txt file in the envMonitor*/
-    type:String,
-    mainID:String, /*main RPi's ID from the server to whom this resMonitor belongs to*/
+    mainID:String, /*main RPi's ID to whom this resMonitor belongs to*/
+    type:String, /*either envMonitor or resMonitor*/
     name:String, /*name of the monitor, assigned by the user*/
     status: Boolean, /*true if connected to the main RPi*/
     lastConnection: Date, /*Last connection to the main RPi*/
-    tempUnit:String, /*Assigned by the user, value can be either 'Celsius' or 'Fahrenheit'*/
-    aTempStatus: Boolean, /*True if the sensor is plugged in and working*/
-    rhStatus: Boolean,  /*True if the sensor is plugged in and working*/
-    co2Status: Boolean, /*True if the sensor is plugged in and working*/
-    luxStatus: Boolean, /*True if the sensor is plugged in and working*/
-    aTempLast: {reading: Number, date: Date, compensated:Boolean}, /*last ambient temperature*/
-    rhLast: {reading: Number, date: Date, compensated:Boolean}, /*last relative humidity reading*/
-    co2Last: {reading: Number, date: Date}, /*last CO2 reading*/
-    luxLast: {reading: Number, date: Date}, /*last Lux*/
-    phCal4: Date, /*Last calibration at pH 4*/
-    phCal7: Date, /*Last calibration at pH 7*/
-    phCal10: Date, /*Last calibration at pH 10*/
-    phStatus: Boolean, /*True if the sensor is plugged in and working*/
-    ecStatus: Boolean,  /*True if the sensor is plugged in and working*/
-    wTempStatus: Boolean, /*True if the sensor is plugged in and working*/
-    usReaderStatus: Boolean, /*True if the sensor is plugged in and working*/
-    phLast: {reading: Number, date: Date, compensated:Boolean}, /*última lectura del pH (ya sea manual o de rutina) y fecha de la última lectura*/
-    ecLast: {reading: Number, date: Date, compensated:Boolean}, /*última lectura del ec (ya sea manual o de rutina) y fecha de la última lectura*/
-    wTempLast: {reading: Number, date: Date}, /*last wTemp*/
-    usReaderLast: {reading: Number, date: Date}, /*last distance reading*/
-    resSize: {height: Number, area: Number}, /*height and area of reservoir, in order to estimate the level*/
-    resLevelLast: Number, /* % of capacity */
+    resSize: {height: {type:Number,default:0}, area:{type:Number,default:0}}, /*height and area of reservoir, set by the user*/
+    tempUnit:{type: String, default:'Celsius'},
+    aTemp:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    rh:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    co2: {status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    lux:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    ph:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number, cal4:Date, cal7:Date, cal10:Date},
+    ec:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    do:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    wTemp:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    usReaderLast: {status: Boolean, lastReading:Number, lastDate:Number}, /*last distance reading*/
+    resLevel: {lastReading:Number, lastDate:Number, lBound:Number, uBound:Number}, /* % of capacity */
 });
 
 module.exports=mongoose.model('Monitor',monitorSchema);
