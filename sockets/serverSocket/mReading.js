2ba21b34ab49d92fdcd72a5fc87e5bf21cfd50ec
jamesokbo <jamesokbo@gmail.com>
2016-10-05 12:53:30 -0500

    Update mReading.js

--- sockets/serverSocket/mReading.js
+++ sockets/serverSocket/mReading.js
@@ -6,51 +6,26 @@ var ResMonitor=require('../../../server/models/resMonitor.js');
 
 module.exports = function(socket){
   socket.on('mReading',function(data,fn){
-    if(data.resMonitorID){
-      if(envVariables.resMonitorsIDs.indexOf(data.resMonitorID)!=-1){
-        envVariables.resMonitors[resMonitorsIDs.indexOf(data.resMonitorID)].emit('mReading',data,function(err,ans){
-          if(err){
-            fn(err);
-          }
-          fn(ans);
-        });
-      }
-      else{
-        ResMonitor.find({resMonitorID:data.resMonitorID},function(err,docs){
-          if(err){
-            throw err;
-          }
-          if(docs.length!=0){
-            fn(errors.m001);
-          }
-          else{
-            fn(errors.m002);
-          }
-        });
-      }
+    if(envVariables.monitorIDs.indexOf(data.monitorID)!=-1){
+      envVariables.monitors[monitorIDs.indexOf(data.monitorID)].emit('mReading',data,function(err,res){
+        if(err){
+          fn(err);
+        }
+        fn(res);
+      });
     }
     else{
-      if(envVariables.envMonitorsIDs.indexOf(data.envMonitorID)!=-1){
-        envVariables.envMonitors[envMonitorsIDs.indexOf(data.envMonitorID)].emit('mReading',data,function(err,ans){
-          if(err){
-            fn(err);
-          }
-          fn(ans);        
-        });
-      }
-      else{
-        EnvMonitor.find({envMonitorID:data.envMonitorID},function(err,docs){
-          if(err){
-            throw err;
-          }
-          if(docs.length!=0){
-            fn(errors.m001);
-          }
-          else{
-            fn(errors.m002);
-          }
-        });
-      }
+      Monitor.find({monitorID:data.monitorID},function(err,docs){
+        if(err){
+          throw err;
+        }
+        if(docs.length!=0){
+          fn(errors.m001);
+        }
+        else{
+          fn(errors.m002);
+        }
+      });
     }
   });
 }
