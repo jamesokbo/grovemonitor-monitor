dbb2e0881b1fcab1c1f2ba207ba679788080f75a
jamesokbo <jamesokbo@gmail.com>
2016-10-05 11:01:05 -0500

    Update connection.js
    Added protocol to send routine readings stuck on mainRPi Side on connection

--- sockets/serverSocket/connection.js
+++ sockets/serverSocket/connection.js
@@ -1,6 +1,9 @@
 var fs=require('fs');
+var async=require('../../../async');
 var constants= require('../../../constants.js')
 var envVariables=require('../../../envVariables.js')
+var Reading=require('../../../server/models/reading.js')
+
 
 module.exports = function(socket){
   socket.on('connect',function(){
@@ -25,6 +28,36 @@ module.exports = function(socket){
           }
           else{
             envVariables.serverConnectionStatus=true;
+            Reading.find({},function(err,docs){
+              if(err){
+                throw err;
+              }
+              var i=0;
+              var j=0;
+              async.whilst(i<docs.length
+                async.whilst(i==j,
+                  socket.emit('rReading',docs[i],function(err,res){
+                    if(err){
+                      throw err;
+                    }
+                    if(res.status){
+                      Reading.findById(docs[i]._id,function(err,doc){
+                        if(err){
+                          throw err;
+                        }
+                        doc.remove(function(err,res){
+                          if(err){
+                            throw err;
+                          }
+                        });
+                      });
+                    i++;
+                    }
+                  });                       
+               );
+               j++;
+              ); 
+            });
           }
         }
         else{
