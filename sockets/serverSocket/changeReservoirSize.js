34c1173a750630541eff6eb86109d089cc19d90a
jamesokbo <jamesokbo@gmail.com>
2016-10-06 11:55:17 -0500

    Update changeReservoirSize.js

--- sockets/serverSocket/changeReservoirSize.js
+++ sockets/serverSocket/changeReservoirSize.js
@@ -6,14 +6,13 @@ var Monitor=require('../../../server/models/monitor.js');
 module.exports = function(socket){
   socket.on('changeReservoirSize',function(data,fn){
     if(envVariables.monitorIDs.indexOf(data.monitorID)!=-1){
-      envVariables.monitors[monitorIDs.indexOf(data.monitorID)].emit('changeReservoirSize',data,function(err,res){
+      envVariables.monitors[envVariables.monitorIDs.indexOf(data.monitorID)].emit('changeReservoirSize',data,function(err,res){
         if(err){
           fn(err);
         }
         Monitor.update({monitorID:data.monitorID},{$set:{'resSize.height':data.height, 'resSize.area':data.area}},function(err,res){
           if(err){
             throw err;
-            fn(res);
           }
         });
       });
