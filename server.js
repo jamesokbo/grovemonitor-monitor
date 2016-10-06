8835314aa2dc6a2b04cc7d5ea5850cb176341399
jamesokbo <jamesokbo@gmail.com>
2016-10-06 16:24:17 -0500

    Update server.js
    serverSocket:
    -Added .on('changeReservoirSize') event
    
    monitorSocket:
    -Added .on('disconnect') event

--- server.js
+++ server.js
@@ -23,14 +23,15 @@ var Monitor=require('./server/models/monitor');
 var serverSocket=require('socket.io-client')(constants.SERVER_URL);
 require('./sockets/serverSocket/connection.js')(serverSocket);
 require('./sockets/serverSocket/mReading.js')(serverSocket);
+require('./sockets/serverSocket/changeReservoirSize.js')(serverSocket);
 
 //TODO: Add SocketIO communication protocol with the monitors
 monitorIO.on('connection', function(monitorSocket){
-  monitorSocket.monID='';
-  monitorSocket.monitor=new Monitor();
+  monitorSocket.monitorID='';
   
   require('./sockets/monitorSockets/identification.js')(monitorSocket);
   require('./sockets/monitorSockets/rReading.js')(monitorSocket);
+  require('./sockets/monitorSockets/disconnect.js')(monitorSocket);
 };
 
 //TODO: Add SocketIO communication protocol with the actuators
