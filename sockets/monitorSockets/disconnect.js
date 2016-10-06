fce045bf10ce6bf9d82e7c267226b97cbd99a569
jamesokbo <jamesokbo@gmail.com>
2016-10-06 16:25:05 -0500

    Update disconnect.js

--- sockets/monitorSockets/disconnect.js
+++ sockets/monitorSockets/disconnect.js
@@ -1,1 +1,30 @@
+var mongoose=require('../../../mongoose');
+var errors=require('../../../errors.js');
+var Monitor=require('../../../server/models/monitor.js');
+var envVariables=require('../../../envVariables.js');
 
+module.exports=function(monitorSocket,serverSocket){
+  monitorSocket.on('disconnect', function(){
+    if(monitorSocket.monitorID!=''){
+      Monitor.update({monitorID:mongoose.Types.ObjectId(monitorSocket.monitorID)},
+      {$set:{status:false, lastConnection:Date.now()}},function(err,res){
+        if(err){
+          throw err;
+        }
+        if(res.ok==1 && res.nModified==1){
+          if(envVariables.monitorIDs.indexOf(monitorSocket.monitorID.toString())!=-1){
+            envVariables.monitors.splice(envVariables.monitorIDs.indexOf(monitorSocket.monitorID.toString()),1);
+            envVariables.monitorIDs.splice(envVariables.monitorIDs.indexOf(monitorSocket.monitorID.toString()),1);
+            if(envVariables.serverConnectionStatus){
+              serverSocket.emit('monitorDisconnect',{monitorID:monitorSocket.monitorID.toString()},function(err,res){
+                if(err){
+                  throw err;
+                }
+              });
+            }
+          }
+        }
+      });
+    }
+  });
+};
