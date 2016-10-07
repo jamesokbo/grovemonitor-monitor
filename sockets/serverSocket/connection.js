37069334af23e7d0ad73e22ced12b96e53a62949
jamesokbo <jamesokbo@gmail.com>
2016-10-07 14:05:08 -0500

    Update connection.js

--- sockets/serverSocket/connection.js
+++ sockets/serverSocket/connection.js
@@ -1,73 +1,18 @@
 var fs=require('fs');
 var async=require('../../../async');
-var constants= require('../../../constants.js')
-var envVariables=require('../../../envVariables.js')
-var Reading=require('../../../server/models/reading.js')
+var constants= require('../../../constants.js');
+var envVariables=require('../../../envVariables.js');
+var Reading=require('../../../server/models/reading.js');
 
 
 module.exports = function(socket){
   socket.on('connect',function(){
-    fs.readFile(constants.MAIN_ID_PATH,'utf8',function(err,data){
+    fs.readFile(constants.MAINRPIID_PATH,'utf8',function(err,data){
       if(err){
         throw err;
       }
-      constants.MAIN_ID=data;
-      socket.emit('identification', {mainRPiID: constants.MAIN_ID}, function(err,res){
-        if(err){
-          throw err;
-        }
-        if(res.status){
-          if(res.new){
-            fs.writeFile(constants.MAIN_ID_PATH, res.mainRPiID, 'utf8', function(err){
-              if(err){
-                throw err;
-              }
-              constants.MAIN_ID=res.id;
-              socket.disconnect();
-            });
-          }
-          else{
-            envVariables.serverConnectionStatus=true;
-            Reading.find({},function(err,docs){
-              if(err){
-                throw err;
-              }
-              var i=0;
-              var j=0;
-              
-              async.whilst(function(){i<docs.length},function(){
-                  async.whilst(function(){i==j},function(){
-                      socket.emit('rReading',docs[i],function(err,res){
-                        if(err){
-                          throw err;
-                        }
-                        if(res.status){
-                          Reading.findById(docs[i]._id,function(err,doc){
-                            if(err){
-                              throw err;
-                            }
-                            doc.remove(function(err,res){
-                              if(err){
-                                throw err;
-                              }
-                            });
-                          });
-                          i++;
-                        }
-                      });
-                  j++;
-                  },function(){});
-              },function(){});
-            });
-          }
-        }
-        else{
-          if(res.error){
-            console.log(res.error);
-          }
-          socket.disconnect();
-        }
-      });
+      require('./identification.js')(socket);
+      constants.MAINRPI_ID=data;
     });
   }); 
   socket.on('disconnect',function(){
