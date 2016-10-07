feae71399e40d6f37d8772e8995170049839c3b6
jamesokbo <jamesokbo@gmail.com>
2016-10-07 13:59:21 -0500

    Create identification.js

---
+++ sockets/serverSocket/identification.js
@@ -0,0 +1,60 @@
+
+
+module.exports=function(socket){
+  socket.emit('identification', {mainRPiID: constants.MAIN_ID}, function(err,res){
+    if(err){
+      throw err;
+    }
+    if(res.status){
+      if(res.new){
+        fs.writeFile(constants.MAIN_ID_PATH, res.mainRPiID, 'utf8', function(err){
+          if(err){
+            throw err;
+          }
+          constants.MAIN_ID=res.id;
+          socket.disconnect();
+        });
+      }
+      else{
+        envVariables.serverConnectionStatus=true;
+        Reading.find({},function(err,docs){
+          if(err){
+            throw err;
+          }
+          var i=0;
+          var j=0;
+          
+          async.whilst(function(){i<docs.length},function(){
+              async.whilst(function(){i==j},function(){
+                  socket.emit('rReading',docs[i],function(err,res){
+                    if(err){
+                      throw err;
+                    }
+                    if(res.status){
+                      Reading.findById(docs[i]._id,function(err,doc){
+                        if(err){
+                          throw err;
+                        }
+                        doc.remove(function(err,res){
+                          if(err){
+                            throw err;
+                          }
+                        });
+                      });
+                      i++;
+                    }
+                   });
+              j++;
+              },function(){});
+          },function(){});
+        });
+      }
+    }
+    else{
+      if(res.error){
+        console.log(res.error);
+      }
+      socket.disconnect();
+    }
+  });
+}
