cb218c136c3ba870841a8c11ef656bb5e5042d1e
jamesokbo <jamesokbo@gmail.com>
2016-10-07 13:18:07 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -2,5 +2,6 @@ module.exports={
   m001: Error('m001: The requested monitor has lost connection'),
   m002: Error('m002: The requested monitor isnt recognized by this system'),
   m003: Error('m003: Unidentified monitor'),
-  m004: Error('m004: Connection to server is down')
+  m004: Error('m004: Connection to server is down'),
+  m005: Error('m005: Couldnt add new monitor to server')
 }
