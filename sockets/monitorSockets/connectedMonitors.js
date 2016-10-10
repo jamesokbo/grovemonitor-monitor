db7c4a6b722c6adab0f696d764ba52ab25b8e029
jamesokbo <jamesokbo@gmail.com>
2016-10-10 14:24:54 -0500

    Create connectedMonitors.js

---
+++ sockets/monitorSockets/connectedMonitors.js
@@ -0,0 +1,14 @@
+var mongoose=require('../../../mongoose');
+var errors=require('../../../errors.js');
+var Monitor=require('../../../server/models/monitor.js');
+var envVariables=require('../../../envVariables.js');
+
+
+module.exports=function(serverSocket){
+
+  var data={monitorIDs:envVariables.monitorIDS};
+  
+  serverSocket.emit('connectedMonitors',function(data, fn){
+    
+  });
+}
