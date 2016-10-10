c20e57b1178f3606fa37dc9210966f93b7951ff6
jamesokbo <jamesokbo@gmail.com>
2016-10-10 14:27:59 -0500

    Update monitorIdentification.js

--- sockets/monitorSockets/monitorIdentification.js
+++ sockets/monitorSockets/monitorIdentification.js
@@ -26,9 +26,6 @@ module.exports=function(socket, serverSocket){
                   if(err){
                     fn(err);
                   }
-                  if(res.status){
-                    require('./connectedMonitors.js')(serverSocket);
-                  }
                   fn(res);
                 });
               }
