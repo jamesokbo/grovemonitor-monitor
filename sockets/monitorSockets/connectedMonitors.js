d49286bb4232964387081e77b296e7c9574bc107
jamesokbo <jamesokbo@gmail.com>
2016-10-10 14:26:07 -0500

    Update connectedMonitors.js

--- sockets/monitorSockets/connectedMonitors.js
+++ sockets/monitorSockets/connectedMonitors.js
@@ -8,7 +8,10 @@ module.exports=function(serverSocket){
 
   var data={monitorIDs:envVariables.monitorIDS};
   
-  serverSocket.emit('connectedMonitors',function(data, fn){
-    
+  serverSocket.emit('connectedMonitors',data,function(err, res){
+    if(err){
+      throw err;
+    }
+    //TODO: success logic
   });
 }
