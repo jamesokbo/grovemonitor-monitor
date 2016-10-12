521f9da3528157ffc4a92fb3f537fe2ff6c84e02
jamesokbo <jamesokbo@gmail.com>
2016-10-12 16:41:41 -0500

    Update mainRPi.js

--- server/models/mainRPi.js
+++ server/models/mainRPi.js
@@ -5,8 +5,7 @@ var mainRPiSchema= mongoose.Schema({
     userID:String, /* The ID of the user to whom this system belongs to */
     name:String, /* Assigned by the user to identify his Grove */
     status: Boolean, /* true if connected to the server */
-    lastConnection: Date,
-    monitorIDs:[String]
+    lastConnection: Date
 });
 
 module.exports=mongoose.model('MainRPi',mainRPiSchema);
