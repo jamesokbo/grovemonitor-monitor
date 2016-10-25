6d00767dbf9f1a5032f0684c78d2a4d36f480eef
jamesokbo <jamesokbo@gmail.com>
2016-10-25 13:01:55 -0500

    Update monitor.js
    All sensors have a String 'status' which can be 'false' if not connected or broken, 'ok' if connected and last reading within the boundaries, 'warning' if connected but last reading outside of boundaries.

--- server/models/monitor.js
+++ server/models/monitor.js
@@ -9,15 +9,15 @@ var monitorSchema= mongoose.Schema({
     lastConnection: Number, /*Date of last connection to the main RPi*/
     resSize: {height: {type:Number,default:0}, area:{type:Number,default:0}}, /*height and area of reservoir, set by the user*/
     tempUnit:{type: String, default:'Celsius'},
-    aTemp:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    rh:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    co2: {status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    lux:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    ph:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number, cal4:Date, cal7:Date, cal10:Date},
-    ec:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    do:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    wTemp:{status: Boolean, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
-    usReader: {status: Boolean, lastReading:Number, lastDate:Number}, /*last distance reading*/
+    aTemp:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    rh:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    co2: {status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    lux:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    ph:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number, cal4:Date, cal7:Date, cal10:Date},
+    ec:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    do:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    wTemp:{status: String, lastReading:Number, lastDate:Number, lBound:Number, uBound:Number},
+    usReader: {status: String, lastReading:Number, lastDate:Number}, /*last distance reading*/
     resLevel: {lastReading:Number, lastDate:Number, lBound:Number, uBound:Number}, /* % of capacity */
 });
 
